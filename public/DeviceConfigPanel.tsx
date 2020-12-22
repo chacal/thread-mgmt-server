dc2f2bd2cc04b3b26901bf8284a5fffb723737c5
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2020-12-22 22:23:56 +0200

    Add UI for controlling device state polling config
    The actual state polling mechanism is not implemented yet

--- public/DeviceConfigPanel.tsx
+++ public/DeviceConfigPanel.tsx
@@ -10,10 +10,23 @@ import AsyncOperationButton from './AsyncOperationButton'
 import StatusMessage, { EmptyStatus } from './StatusMessage'
 import isEqual from 'lodash/isEqual'
 import { makeStyles } from '@material-ui/core/styles'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import Typography from '@material-ui/core/Typography'
 
 const useStyles = makeStyles((theme) => ({
   configPanelRow: {
     marginBottom: theme.spacing(1)
+  },
+  pollIntervalSelectAdornment: {
+    position: 'absolute',
+    padding: 0,
+    right: '16px',
+    top: 'calc(50%)',
+  },
+  checkBoxRoot: {
+    padding: theme.spacing(0.5, 1)
   }
 }))
 
@@ -25,6 +38,10 @@ export default function DeviceConfigPanel(props: { config: DeviceConfig, address
   const [status, setStatus] = useState(EmptyStatus)
 
   const onMainIPSelected = (mainIp: string) => setConfig({ ...config, mainIp })
+
+  const onPollConfigChanged = (enabled: boolean, interval: number) => {
+    setConfig({ ...config, statePollingEnabled: enabled, statePollingIntervalSec: interval })
+  }
 
   const onClickSave = () => {
     setStatus(EmptyStatus)
@@ -39,6 +56,14 @@ export default function DeviceConfigPanel(props: { config: DeviceConfig, address
       <Grid item xs={12} sm={9} md={10} lg={8}>
         <MainIPSelect mainIp={config.mainIp} addresses={addresses} onMainIPSelected={onMainIPSelected}/>
       </Grid>
+    </Grid>
+    <Grid item container spacing={3} className={classes.configPanelRow}>
+      <StatePollingControls
+        disabled={config.mainIp === ''}
+        pollingEnabled={config.statePollingEnabled}
+        pollingIntervalSec={config.statePollingIntervalSec}
+        onPollChange={onPollConfigChanged}
+      />
     </Grid>
     <Grid item container spacing={2} xs={12}>
       <Grid item>
@@ -63,3 +88,45 @@ function MainIPSelect(props: { mainIp: string, addresses: string[], onMainIPSele
   </FormControl>
 }
 
+function StatePollingControls(props: { disabled: boolean, pollingEnabled: boolean, pollingIntervalSec: number, onPollChange: (enabled: boolean, interval: number) => void }) {
+  const classes = useStyles()
+
+  const onPollingEnabledChange = (e: ChangeEvent<HTMLInputElement>) => {
+    props.onPollChange(e.target.checked, props.pollingIntervalSec)
+  }
+  const onPollIntervalChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    props.onPollChange(props.pollingEnabled, parseInt(e.target.value))
+  }
+
+  return <Grid item container xs={12} sm={9} md={10} lg={8} alignItems={'flex-end'}>
+    <Grid item xs={12}>
+      <Typography variant={'caption'} color={'textSecondary'}>State polling</Typography>
+    </Grid>
+    <Grid item container xs={12}>
+      <Grid item container xs={5}>
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={
+              <Checkbox disabled={props.disabled} checked={props.pollingEnabled} onChange={onPollingEnabledChange}
+                        classes={{ root: classes.checkBoxRoot }}/>
+            }
+            label="Enabled"
+          />
+        </Grid>
+      </Grid>
+      <Grid item xs={3}>
+        <FormControl fullWidth>
+          <Select labelId="label" value={props.pollingIntervalSec}
+                  disabled={props.disabled || !props.pollingEnabled} onChange={onPollIntervalChange}
+                  endAdornment={
+                    <InputAdornment position="start"
+                                    className={classes.pollIntervalSelectAdornment}>min</InputAdornment>
+                  }
+          >
+            {[1, 2, 3, 5, 10, 15, 20, 25, 30].map(v => <MenuItem key={v} value={v * 60}>{v}</MenuItem>)}
+          </Select>
+        </FormControl>
+      </Grid>
+    </Grid>
+  </Grid>
+}
